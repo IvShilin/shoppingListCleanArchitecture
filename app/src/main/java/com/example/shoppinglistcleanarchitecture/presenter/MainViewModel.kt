40c518c8b59e321474b4798e7a9d91d7886b4d25
Ivan Shilin <68341349+IvShilin@users.noreply.github.com>
2022-01-30 07:46:18 +0300

    Added getting of list in MainViewModel

---
+++ app/src/main/java/com/example/shoppinglistcleanarchitecture/presenter/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.shoppinglistcleanarchitecture.presenter
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglistcleanarchitecture.data.ShoppingListRepositoryImplements
+import com.example.shoppinglistcleanarchitecture.domain.DeleteShoppingItemUseCase
+import com.example.shoppinglistcleanarchitecture.domain.EditShoppingItemUseCase
+import com.example.shoppinglistcleanarchitecture.domain.GetShoppingListUseCase
+import com.example.shoppinglistcleanarchitecture.domain.ShoppingItem
+
+class MainViewModel : ViewModel() {
+    private val repository = ShoppingListRepositoryImplements
+
+    private val getShoppingListUseCase = GetShoppingListUseCase(repository)
+    private val deleteShoppingItemUseCase = DeleteShoppingItemUseCase(repository)
+    private val editShoppingItemUseCase = EditShoppingItemUseCase(repository)
+
+    var shoppingList = MutableLiveData<List<ShoppingItem>>()
+
+    fun getShoppingList(){
+        val list = getShoppingListUseCase.getShoppingList()
+        shoppingList.value = list
+    }
+}
