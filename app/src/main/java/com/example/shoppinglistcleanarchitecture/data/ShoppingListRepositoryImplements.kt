3bf5652e79a88987e7ce23d62f1dbe1047806c2a
Ivan Shilin <68341349+IvShilin@users.noreply.github.com>
2022-01-30 04:37:09 +0300

    Data pac is eddited (implements repository interface)

--- app/src/main/java/com/example/shoppinglistcleanarchitecture/data/ShoppingListRepositoryImplements.kt
+++ app/src/main/java/com/example/shoppinglistcleanarchitecture/data/ShoppingListRepositoryImplements.kt
@@ -1,7 +1,35 @@
 package com.example.shoppinglistcleanarchitecture.data
 
+import com.example.shoppinglistcleanarchitecture.domain.ShoppingItem
 import com.example.shoppinglistcleanarchitecture.domain.ShoppingListRepository
+import java.lang.RuntimeException
 
 object ShoppingListRepositoryImplements: ShoppingListRepository{
+    private val shoppingList = mutableListOf<ShoppingItem>()
+    private var autoIncrementId = 0
 
+    override fun addShoppingItem(item: ShoppingItem) {
+        if (item.id == ShoppingItem.UNDEFIND_ID) { item.id = autoIncrementId++ }
+        shoppingList.add(item)
+    }
+
+    override fun deleteShoppingItem(item: ShoppingItem) {
+        shoppingList.remove(item)
+    }
+
+    override fun editShoppingItemCase(item: ShoppingItem) {
+        val oldItem = getShoppingItem(item.id)
+        shoppingList.remove(oldItem)
+        addShoppingItem(item)
+    }
+
+    override fun getShoppingItem(id: Int): ShoppingItem {
+        return shoppingList.find { it.id == id }
+            ?:
+            throw RuntimeException("Shopping list id $id is null")
+    }
+
+    override fun getShoppingList(): List<ShoppingItem> {
+        return shoppingList.toList()
+    }
 }
