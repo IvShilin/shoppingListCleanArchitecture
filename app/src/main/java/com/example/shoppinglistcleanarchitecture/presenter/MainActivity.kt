ae09ccc571f26c2a8254ef96856628bd151553c1
Ivan Shilin <68341349+IvShilin@users.noreply.github.com>
2022-01-30 07:47:06 +0300

    MainViewModel pinned to MainActivity and tested with logging

--- app/src/main/java/com/example/shoppinglistcleanarchitecture/presenter/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistcleanarchitecture/presenter/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.shoppinglistcleanarchitecture.presenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglistcleanarchitecture.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        viewModel.shoppingList.observe(this){
+            Log.d("MainViewModel", it.toString())
+        }
+
+        viewModel.getShoppingList()
     }
 }
